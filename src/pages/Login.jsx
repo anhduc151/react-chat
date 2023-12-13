868681d2ca3d11c2898fa5fb4b9d8dda43efa83e
anhduc1510 <gametamin.member3@gmail.com>
2023-12-13 10:43:24 +0700

    update message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
+import { message } from "antd";
 
 const Login = () => {
-  const [err, setErr] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -14,23 +14,27 @@ const Login = () => {
 
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate("/")
-    } catch (err) {
-      setErr(true);
+      message.success("Login Successfully");
+      setTimeout(function () {
+        navigate("/");
+      }, 1000);
+    } catch (error) {
+      message.error("Failed to login");
     }
   };
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">Message</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
           <button>Sign in</button>
-          {err && <span>Something went wrong</span>}
         </form>
-        <p>You don't have an account? <Link to="/register">Register</Link></p>
+        <p>
+          You don't have an account? <Link to="/register">Register</Link>
+        </p>
       </div>
     </div>
   );
